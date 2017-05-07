b125eea255b28226d9980c1428f30a13ebf500a7
Gabriel <gabco96@hotmail.com>
2017-05-07 18:34:33 +0200

    Test

---
+++ VP_Proekt/VP_Proekt/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VP_Proekt
+{
+    class Config
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public int num_levels { get; set; }
+        public enum GameDifficulty : int
+        {
+            EASY,
+            MEDIUM,
+            HARD
+        };
+        public GameDifficulty selectedGameDifficulty;
+        public Config(int width = 800, int height = 600,int num_levels = 0, GameDifficulty GameDifficulty = GameDifficulty.EASY)
+        {
+            this.width = width;
+            this.height = height;
+            this.num_levels = num_levels;
+            this.selectedGameDifficulty = GameDifficulty;
+        }
+        public void increaseNumLevels(){
+            this.num_levels++;
+        }
+
+    }
+}
